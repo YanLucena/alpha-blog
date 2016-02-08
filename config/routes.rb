71d9e5aab89b7e0c49b97a4b72975c16f1066db2
yanlucena <yangoestocollege@gmail.com>
2016-02-08 22:31:27 +0000

    Sign up users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-root "pages#home"
+root 'pages#home'
 
-get "about", to: "pages#about"
+get 'about', to: 'pages#about'
 
 resources :articles
+
+get 'signup', to: 'users#new'
+resources :users, except: [:new]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
